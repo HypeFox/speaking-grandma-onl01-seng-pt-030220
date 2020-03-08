fd1dddf050a8668c61a237e39a9de354fc993bbb
Brandon Guffey <hypefox@gmail.com>
2020-03-08 20:10:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-else 
-  return answer
-end
+  else 
+    return answer
+  end
 end
